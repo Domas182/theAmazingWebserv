49533531ae7663cf0036d68afa0a3931129f9841
Sonja Reinholz <sreinhol@3-C-6.42heilbronn.de>
2022-11-21 21:24:22 +0100

    FINAL

--- Crazy_Cats/obj/Poller.d
+++
@@ -1,8 +0,0 @@
-obj/Poller.o: src/Poller.cpp src/../incl/Poller.hpp \
-  src/../incl/CrazyCats.hpp src/../incl/Colors.hpp
-
-src/../incl/Poller.hpp:
-
-src/../incl/CrazyCats.hpp:
-
-src/../incl/Colors.hpp:
